db9f4b07c4f24ae1afe12d8b11d0c32f6c356969
nicholas <nicholasovunda@gmail.com>
2022-06-07 21:46:46 +0100

    IMplemented checks for operators and values

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,11 +2,57 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+//import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+    var lastNumeric : Boolean =  false
+    var lastDot : Boolean =  false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        tvInput = findViewById(R.id.tvInput)
+    }
+    fun onDigit(view: View){
+        tvInput?.append((view as Button).text)
+        lastDot = false
+        lastNumeric = true
+    }
+    fun onClear(view: View){
+        tvInput?.text = ""
+    }
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+        tvInput?.append(".")
+        lastNumeric = false
+        lastDot = true
+        }
+    }
+    fun onOperator(view: View){
+        tvInput?.text?.let{
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastDot = false
+                lastNumeric = false
+            }
+        }
+    }
+    fun onEqual(view : View){
+        if(lastNumberic){
+            var tvValue
+        }
+    }
+    private fun isOperatorAdded(value : String) : Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            (value.contains("-"))|| (value.contains("+"))|| (value.contains("*"))|| (value.contains("/"))
+        }
+    }
 }
